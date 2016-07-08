68453b784574939e0db4a9521709a1ae3d295b27
Federico Kauffman <federico@wyeworks.com>
2016-07-08 14:42:32 -0300

    Ok assertion transformation

---
+++ tests/fixtures/original/ok.js
@@ -0,0 +1,23 @@
+import { testHelper } from 'shell/helpers/test-helper';
+import { module, test } from 'qunit';
+
+function testFunction(a) {
+  return a;
+}
+
+module('Unit | Helper | test helper');
+
+//Replace this with your real tests.
+test('it works', function(assert) {
+  let result = testHelper([42]),
+      obj = { a: true, b: false };
+
+  assert.ok(result);
+  assert.ok((function() { return true; })());
+  assert.ok(1===1, 'testing equality');
+  assert.ok(true);
+  assert.ok((1+2+3)-3*5*6);
+  assert.ok(function() { return true; }(), 'testing a function');
+  assert.ok(obj.a);
+  assert.ok(testFunction(true));
+});
--- tests/unit/lib/transform-qunit-assertions-test.js
+++ tests/unit/lib/transform-qunit-assertions-test.js
@@ -1,8 +1,12 @@
 var transform = require('../../../lib/transform-qunit-assertions');
 var assert = require('chai').assert;
+var fs = require('fs');
 
 describe('transform qunit assertions', function() {
-  it('transforms source to ok', function() {
-    assert.equal(transform('source'), 'ok');
+  it('transforms: adds message to ok assertion without message', function() {
+    var source = fs.readFileSync('./tests/fixtures/original/ok.js', 'utf8');
+    var transformed = fs.readFileSync('./tests/fixtures/transformed/ok.js', 'utf8');
+
+    assert.equal(transform(source), transformed);
   });
 });
