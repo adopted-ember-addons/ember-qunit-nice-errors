4772bbe5d7e152de38ffab81f5c61358209e0fbb
Diego Acosta <diego@wyeworks.com>
2016-07-12 17:48:51 -0300

    Apply transformation to tests on build

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 /* jshint node: true */
 'use strict';
 
+const TestTransformFilter = require('./lib/tests-transform-filter');
+
 module.exports = {
-  name: 'ember-cli-qunit-nice-errors'
+  name: 'ember-cli-qunit-nice-errors',
+
+  postprocessTree(type, tree) {
+    if (type === 'test') {
+      return new TestTransformFilter(tree);
+    }
+    return tree;
+  }
 };
