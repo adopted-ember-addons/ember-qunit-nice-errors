b5eb039b393cf8b79b0fa79cf4be878bb4a66927
Santiago Ferreira <santiago.ferreira@wyeworks.com>
2016-07-14 19:35:07 -0300

    Add notOk transformation

--- node-tests/transform-qunit-assertions-test.js
+++ node-tests/transform-qunit-assertions-test.js
@@ -4,11 +4,19 @@ var transform = require('../lib/transform-qunit-assertions');
 var assert = require('chai').assert;
 var fs = require('fs');
 
+function assertOutput(fileName) {
+  var source = fs.readFileSync('./node-tests/fixtures/original/' + fileName + '.js', 'utf8');
+  var transformed = fs.readFileSync('./node-tests/fixtures/transformed/' + fileName + '.js', 'utf8');
+
+  assert.equal(transform(source), transformed);
+}
+
 describe('transform qunit assertions', function() {
   it('transforms: adds message to ok assertion without message', function() {
-    var source = fs.readFileSync('./node-tests/fixtures/original/ok.js', 'utf8');
-    var transformed = fs.readFileSync('./node-tests/fixtures/transformed/ok.js', 'utf8');
+    assertOutput('ok');
+  });
 
-    assert.equal(transform(source), transformed);
+  it('transforms: adds message to notOk assertion without message', function() {
+    assertOutput('not-ok');
   });
 });
