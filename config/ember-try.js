fc0fa0cb7df375ff92aed7c9da7537382313d191
Samanta de Barros <samanta@wyeworks.com>
2017-09-02 20:29:09 +0200

    Trying to fix ember-try not working on travis

--- config/ember-try.js
+++ config/ember-try.js
@@ -43,25 +43,49 @@ module.exports = {
     },
     {
       name: 'ember-release',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#release'
+        },
+        resolutions: {
+          'ember': 'release'
+        }
+      },
       npm: {
         devDependencies: {
-          'ember-source': 'components/ember#release',
+          'ember-source': null
         }
       }
     },
     {
       name: 'ember-beta',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#beta'
+        },
+        resolutions: {
+          'ember': 'beta'
+        }
+      },
       npm: {
         devDependencies: {
-          'ember-source': 'components/ember#beta',
+          'ember-source': null
         }
       }
     },
     {
       name: 'ember-canary',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#canary'
+        },
+        resolutions: {
+          'ember': 'canary'
+        }
+      },
       npm: {
         devDependencies: {
-          'ember-source': 'components/ember#canary',
+          'ember-source': null
         }
       }
     },
